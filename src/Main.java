06d918b34986c9bd320c6cc872853606d4d50fa3
AlexanderGaltsov <gau051177@gmail.com>
2022-11-06 13:50:09 +0300

    Добавлена ветка eraser, использован Predicate

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,25 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
+
 
 public class Main {
+
     public static void main(String[] args){
         List<Person> people = new ArrayList<>();
         people.add(new Person("Семён", "Захаров", 23));
+        people.add(new Person("Денис", "Каруселькин", 8));
         people.add(new Person("Анна", "Захарова Попова", 28));
+        people.add(new Person("Любовь", "Обручева Круглая", 15));
         people.add(new Person("Артем", "Эк Зю Пери", 27));
+        people.add(new Person("Ольга", "Семенова", 17));
         people.add(new Person("Антон", "Весёлый Колобок", 35));
 
-        Collections.sort(people, new PersonComparator(2));
+
+        Predicate<Person> lessThan = person -> person.getAge() < 18;
+        people.removeIf(lessThan);
+
         System.out.println(people);
 
     }
-
 }
--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,5 @@
+import java.util.function.Predicate;
+
 public class Person {
 
 
@@ -12,29 +14,14 @@ public class Person {
         this.age = age;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getSurname() {
         return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
     }
 
     public int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
     @Override
     public String toString() {
         return  name + " " + surname + " " + age;
